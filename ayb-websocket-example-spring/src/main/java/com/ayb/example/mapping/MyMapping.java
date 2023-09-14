92f9d0c269b234c1116561ba7098f9b0944ad812
{{ALittleGarbage}} <{{2691936018@qq.com}}>
2023-09-14 13:34:42 +0800

    添加test注释

--- ayb-websocket-example-spring/src/main/java/com/ayb/example/mapping/MyMapping.java
+++ ayb-websocket-example-spring/src/main/java/com/ayb/example/mapping/MyMapping.java
@@ -13,6 +13,12 @@ import org.springframework.stereotype.Component;
 @WebSocket(port = 9999, path = "/ws")
 public class MyMapping extends AbstractWebSocketMapping<String, String> {
 
+    /**
+     * 接收消息
+     * @param ctx
+     * @param request
+     * @return
+     */
     @Override
     protected String onMessage(ChannelHandlerContext ctx, String request) {
         // 发送事件
@@ -20,26 +26,50 @@ public class MyMapping extends AbstractWebSocketMapping<String, String> {
         return "Hello, WebSocket";
     }
 
+    /**
+     * 接收二进制消息
+     * @param ctx
+     * @param content
+     * @return
+     */
     @Override
     protected byte[] onBinary(ChannelHandlerContext ctx, byte[] content) {
         return null;
     }
 
+    /**
+     * 客户端下线
+     * @param ctx
+     */
     @Override
     protected void onClose(ChannelHandlerContext ctx) {
         System.out.println("客户端" + ctx.channel().remoteAddress() + "下线");
     }
 
+    /**
+     * 客户端上线
+     * @param ctx
+     */
     @Override
     protected void onOpen(ChannelHandlerContext ctx) {
         System.out.println("客户端" + ctx.channel().remoteAddress() + "上线");
     }
 
+    /**
+     * 错误处理
+     * @param ctx
+     * @param cause
+     */
     @Override
     protected void onError(ChannelHandlerContext ctx, Throwable cause) {
         System.out.println("出现错误,原因:" + cause.getMessage());
     }
 
+    /**
+     * 监听事件
+     * @param ctx
+     * @param evt
+     */
     @Override
     protected void onEvent(ChannelHandlerContext ctx, Object evt) {
         if (evt instanceof String) {
